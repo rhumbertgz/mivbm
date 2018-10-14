ea25ea2339a91d58b92f8c280b003358c266d4a2
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-10-14 17:08:45 +0200

    Update coordinates response structure

--- lib/coordinates_manager.ex
+++ lib/coordinates_manager.ex
@@ -4,7 +4,7 @@ defmodule MIVBM.CoordinatesManager do
   @module Atom.to_string(__MODULE__) |> String.split_at(7) |> elem(1)
 
   def load_coordinates(line) do
-    case :file.consult("coordinates/line-#{line}.lc") do
+    case :file.consult("coordinates/line-#{inspect line}.lc") do
         {:ok, [coordinates]} -> coordinates
         _               -> %{}
     end
@@ -12,21 +12,20 @@ defmodule MIVBM.CoordinatesManager do
 
   def get_coordinates_by_line(coordinates,line, %VehiclePosition{ directionId: direction, pointId: point, distanceFromPoint: 0}) do
     Logger.debug("#{@module}.get_coordinates_by_line line: #{inspect line} direction: #{inspect direction} point: #{inspect point} distance: 0")
-    stop = coordinates
+    {coord , _, _} = coordinates
       |> Map.get(line)
       |> Map.get(direction)
       |> Map.get(point)
-      |> elem(0)
-    %{coordinates: stop, type: 0, capacity: 0}
+    %{coordinates: coord, type: 1, capacity: 0}
   end
 
   def get_coordinates_by_line(coordinates, line, %VehiclePosition{ directionId: direction, pointId: point, distanceFromPoint: distance}) do
     Logger.debug("#{@module}.get_coordinates_by_line line: #{inspect line} direction: #{inspect direction} point: #{inspect point} distance: #{inspect distance}")
-    stop = coordinates
+    {_ , coord, type} = coordinates
       |> Map.get(line)
       |> Map.get(direction)
       |> Map.get(point)
-      |> elem(1)
-    %{coordinates: stop, type: 1, capacity: 0}
+
+    %{coordinates: coord, type: type, capacity: 0}
   end
 end
