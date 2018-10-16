5995f7aef0e08519db85fda5433c16be8d17597f
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-10-16 23:57:05 +0200

    Log missing partial directions

--- lib/coordinates_manager.ex
+++ lib/coordinates_manager.ex
@@ -12,30 +12,35 @@ defmodule MIVBM.CoordinatesManager do
 
   def get_coordinates_by_line(coordinates,line, %VehiclePosition{ directionId: direction, pointId: point, distanceFromPoint: 0}) do
     Logger.debug("#{@module}.get_coordinates_by_line line: #{inspect line} direction: #{inspect direction} point: #{inspect point} distance: 0")
-    {coord , _, _} = coordinates
-      |> Map.get(line, %{})
-      |> Map.get(direction, %{})
-      |> Map.get(point, {[0,0],0,0})
-    log_missing_direction_stop(coord, line, direction, point, 0)
-    %{coordinates: coord, type: 1, capacity: 0}
+    try do
+      {coord , _, _} = coordinates
+                      |> Map.get(line)
+                      |> Map.get(direction)
+                      |> Map.get(point)
+      %{coordinates: coord, type: 1, capacity: 0}
+    rescue
+      _ -> log_missing_direction_stop(line, direction, point, 0)
+    end
   end
 
   def get_coordinates_by_line(coordinates, line, %VehiclePosition{ directionId: direction, pointId: point, distanceFromPoint: distance}) do
     Logger.debug("#{@module}.get_coordinates_by_line line: #{inspect line} direction: #{inspect direction} point: #{inspect point} distance: #{inspect distance}")
-    {_ , coord, type} = coordinates
-    |> Map.get(line, %{})
-      |> Map.get(direction, %{})
-      |> Map.get(point, {0,[0,0],0})
-    log_missing_direction_stop(coord, line, direction, point, distance)
-    %{coordinates: coord, type: type, capacity: 0}
+    try do
+      {_ , coord, type} = coordinates
+                          |> Map.get(line)
+                          |> Map.get(direction)
+                          |> Map.get(point)
+      %{coordinates: coord, type: type, capacity: 0}
+    rescue
+      _ -> log_missing_direction_stop(line, direction, point, distance)
+    end
   end
 
 
-  def log_missing_direction_stop([0,0], line, direction, point, distance) do
+  defp log_missing_direction_stop(line, direction, point, distance) do
     content = :io_lib.format("~tp.~n", ["line: #{inspect line} direction: #{inspect direction} point: #{inspect point} distance: #{inspect distance}"])
     :file.write_file("logs/mivbm#{System.monotonic_time}.err", content)
+    %{coordinates: [0,0], type: 1, capacity: 0}
   end
-  def log_missing_direction_stop(_,_,_,_,_) do
-    :ok
-  end
+
 end
